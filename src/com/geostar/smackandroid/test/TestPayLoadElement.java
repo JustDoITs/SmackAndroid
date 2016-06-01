e1658234680f19491efe54f1e6c275baa6766530
hanlyjiang <hanlyjiang@outlook.com>
2016-06-01 17:26:37 +0800

    test

---
+++ src/com/geostar/smackandroid/test/TestPayLoadElement.java
@@ -0,0 +1,26 @@
+package com.geostar.smackandroid.test;
+
+import org.jivesoftware.smack.packet.ExtensionElement;
+
+
+public class TestPayLoadElement implements ExtensionElement {
+
+	@Override
+	public String getElementName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CharSequence toXML() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getNamespace() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
